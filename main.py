fd90c774a8c8128173209bbea53568690a794afd
ileanarunge-web <ileana.runge@gmail.com>
2025-08-12 10:24:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import json, requests
-from ics import Calendar
+# Génère un ICS minimal valide. On branchera le vrai parseur Celcat ensuite.
 
-# Charger la config
-with open("config.json") as f:
-    config = json.load(f)
+from datetime import datetime
 
-url = config["uvsq_url"]
+ICS = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//UVSQ Exporter//FR
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+NAME:UVSQ DFASM1
+X-WR-CALNAME:UVSQ DFASM1
+X-WR-TIMEZONE:Europe/Paris
+X-ORIGINAL-GENERATED:{stamp}
+END:VCALENDAR
+""".replace("\n", "\r\n").format(stamp=datetime.utcnow().strftime("%Y%m%dT%H%M%SZ"))
 
-# Récupérer la page HTML (ici simplifié, le vrai script doit parser Celcat)
-# Dans une vraie version, on extraierait les événements et on les transformerait en .ics
-# Pour ce pack, on met un exemple minimal
-
-cal = Calendar()
-# TODO: Ajouter la logique de parsing de Celcat ici
-
-with open("emploi.ics", "w") as f:
-    f.writelines(cal)
+with open("emploi.ics", "w", encoding="utf-8") as f:
+    f.write(ICS)
